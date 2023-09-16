f0d93a8b303420755acd8346b59d69251055d244
Zeleif <alyomeguja@gmail.com>
2023-09-16 11:22:58 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/Home';
+import About from './pages/About';
+import Error from './pages/Error';
 
 
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='*' element={<Error />} />
+
+            </Routes>
+        </BrowserRouter>
 
     );
 };
